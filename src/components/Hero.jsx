0ff3668190ae1c89b682d547fd911faf6e57b085
c4niss <133370112+c4niss@users.noreply.github.com>
2024-02-17 00:49:01 +0100

    hero page added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import { motion } from "framer-motion";
+import {styles} from '../styles';
+import ComputersCanvas from './canvas/Computers'
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className=' relative w-full h-screen  mx-auto '>
+      <div className={`${styles.paddingX} absolute inset-0 top-[100px] mx-auto flex flex-row max-w-7xl items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className=' w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+        <div className=''>
+            <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className=' text-[#915eff]'>anis</span></h1>
+            <p className={`${styles.heroSubText}`}>
+              I am <span>a fron end developer using react and tailwind still a beginner</span>
+            </p>
+          </div>
+      </div>
+      <ComputersCanvas />    
+      
+    </section>
+    )
 }
 
 export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
-import { styles} from '../style'
+import { styles} from '../styles'
 import {navLinks} from '../constants'
 import {logo , menu , close} from '../assets'
 function Navbar() {
@@ -9,7 +9,7 @@ function Navbar() {
 
   return (
     <nav
-    className={'${style.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary '}
+    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link   
@@ -20,7 +20,7 @@ function Navbar() {
           } }
         >
           <img src={logo} alt="logo" className=' w-9 h-9 object-contain ' />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Anis <span className=" sm:block hidden">| Javascript mastery</span></p>  
+          <p className="text-white text-[18px] font-bold cursor-pointer">Anis <span className=" sm:inline-block">| Javascript mastery</span></p>  
         </Link>
         <ul className=' list-none hidden sm:flex flex-row gap-10 '>
           {navLinks.map((link) =>(
